3d505319c5122ae298f569650e07da7a128cb5f8
Andrew <nonameznoone@gmail.com>
2021-06-06 01:17:32 +0300

    fixed mistakes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var debounce = require('lodash.debounce');
 import '@pnotify/core/dist/PNotify.css';
 import { error } from '@pnotify/core/dist/PNotify.js';
-
 import fetchCountries from './fetchCountries.js';
 import countryMarkupTpl from './country-markup.hbs';
 import countryListTpl from './country-list.hbs';
@@ -13,27 +12,28 @@ const DEBOUNCE_DELAY = 500;
 
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-function onInput(){
+function onInput() {
     let searchQuery = inputEl.value;
     
     resultsContainer.innerHTML = '';
-    if (inputEl.value !== '' && inputEl.value !== ' ' && inputEl.value !== '.'){
+    inputEl.value = inputEl.value.trim();
+    if (inputEl.value !== '' && inputEl.value !== ' ' && inputEl.value !== '.') {
         fetchCountries(searchQuery).then(data => {
             if (data.status === 404) {
                 notificationMessage('Sorry, nothing found.')
             }
             else if (data.length > 10) {
                 notificationMessage('Too many matches found. Please enter a more specific query!');
-                }
+            }
             else if (data.length === 1) {
                 const resultsMarkup = createMenuItemsMarkup(data);
                 resultsContainer.insertAdjacentHTML('beforeend', resultsMarkup);
             }
-            else if (2 <= data.length <= 10){
+            else if (2 <= data.length <= 10) {
                 const resList = createItemsList(data);
-                resultsContainer.insertAdjacentHTML('beforeend', resList);        
+                resultsContainer.insertAdjacentHTML('beforeend', resList);
             }
-    })
+        })
     }
 };
 
